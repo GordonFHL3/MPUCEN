5ce7ee7bf653136e21861ed44d1ffebb20396d46
Maximiliano Perez <gordonfhl3@gmail.com>
2020-06-09 17:12:22 -0400

    Se definen variables

--- Prueba 1/TDA_ListaEnlazada.c
+++ Prueba 1/TDA_ListaEnlazada.c
@@ -1,1 +1,9 @@
 //Link de github: https://github.com/GordonFHL3/MPUCEN
+
+float busqueda_binaria(int dato, struct lista entrada) {
+
+	float centro_lista = (float)fin(entrada);
+	int i;
+	int encontrado = 0;
+
+}
