4b9352a409ed02a2b340485aa895791be83d2116
Maximiliano Perez <gordonfhl3@gmail.com>
2020-06-09 17:19:01 -0400

    Se comienza el desarrollo del algoritmo

--- Prueba 1/TDA_ListaEnlazada.c
+++ Prueba 1/TDA_ListaEnlazada.c
@@ -2,8 +2,19 @@
 
 float busqueda_binaria(int dato, struct lista entrada) {
 
-	float centro_lista = (float)fin(entrada);
+	float fin_lista = (float)fin(entrada);
+	int centro_lista;
+	int dato_aux;
+	int centro_dato;
 	int i;
 	int encontrado = 0;
 
+	if ((fin_lista / 2) == (int)fin_lista) {
+		centro_lista = fin_lista / 2;
+		centro_dato = recuperar(centro_lista, entrada);
+		for (i = 0; i < centro_lista; i++) {
+			dato_aux = recuperar(i, entrada);
+			if(centro_dato)
+		}
+	}
 }
