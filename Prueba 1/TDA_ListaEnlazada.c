5206e652e92c867b7525b3a1ef9f647f7af0b9f4
Maximiliano Perez <gordonfhl3@gmail.com>
2020-06-09 17:26:37 -0400

    Se sigue desarrollando.

--- Prueba 1/TDA_ListaEnlazada.c
+++ Prueba 1/TDA_ListaEnlazada.c
@@ -3,18 +3,32 @@
 float busqueda_binaria(int dato, struct lista entrada) {
 
 	float fin_lista = (float)fin(entrada);
+	int inic_lista = primero(entrada);
 	int centro_lista;
 	int dato_aux;
 	int centro_dato;
 	int i;
 	int encontrado = 0;
 
-	if ((fin_lista / 2) == (int)fin_lista) {
-		centro_lista = fin_lista / 2;
-		centro_dato = recuperar(centro_lista, entrada);
-		for (i = 0; i < centro_lista; i++) {
-			dato_aux = recuperar(i, entrada);
-			if(centro_dato)
+
+	while (centro_lista>=){
+		if ((fin_lista / 2) == (int)fin_lista) {
+			centro_lista = fin_lista / 2;
+			centro_dato = recuperar(centro_lista, entrada);
+			if (centro_dato == dato) {
+				encontrado = 0;
+			}
+
+			else if (centro_dato < dato) {
+
+			}
+
+			/*for (i = 0; i < centro_lista; i++) {
+				dato_aux = recuperar(i, entrada);
+				if (centro_dato == dato_aux) {
+
+				}
+			}*/
 		}
 	}
 }
