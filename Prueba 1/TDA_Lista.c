f18af9d40d476eba3cd57286d9b3aab29c89aa70
Maximiliano Perez <gordonfhl3@gmail.com>
2020-06-09 16:51:43 -0400

    Inicializacion de variables

---
+++ Prueba 1/TDA_Lista.c
@@ -0,0 +1,15 @@
+float promedio(struct lista entrada) {
+	//Para recorrer la lista con un ciclo
+	int ultimo = fin(entrada);
+	//Para dividir la suma de los datos
+	float ultimo_div = (float)ultimo;
+	//Para realizar un ciclo For
+	int i;
+	//Para guardar la suma de los datos
+	float suma = 0;
+	//Para retornar el promedio
+	float salida = 0;
+
+
+
+}
